c66d3f355128f9c595fb6e5aa4a83612c6620d7d
Wonyoung Kang <onezerokang@gmail.com>
2023-07-21 21:24:16 +0900

    WebSocketConfig를 수정하여 STOMP 적용

--- src/main/java/com/chat/config/WebSocketConfig.java
+++ src/main/java/com/chat/config/WebSocketConfig.java
@@ -3,20 +3,22 @@ package com.chat.config;
 import com.chat.handler.ChatHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.*;
 
 /* WebSocket 설정 */
 @Configuration
-@RequiredArgsConstructor
-@EnableWebSocket // 웹소켓 활성화
-public class WebSocketConfig implements WebSocketConfigurer {
-    private final ChatHandler chatHandler;
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        // 웹소켓 접속을 위한 엔드포인트 등록
-        registry.addHandler(chatHandler, "/ws/chat").setAllowedOrigins("*");
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws/chat").withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic"); // sub
+        config.setApplicationDestinationPrefixes("/app"); // pub
     }
 }
