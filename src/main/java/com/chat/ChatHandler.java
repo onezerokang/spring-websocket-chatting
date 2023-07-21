7cda1d80d529eb58a819f91b463ce658ab845bfd
Wonyoung Kang <onezerokang@gmail.com>
2023-07-21 17:21:52 +0900

    최소 기능 채팅 애플리케이션

---
+++ src/main/java/com/chat/ChatHandler.java
@@ -0,0 +1,43 @@
+package com.chat;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/* 클라이언트가 보낸 메시지를 처리할 핸들러 */
+@Component
+public class ChatHandler extends TextWebSocketHandler {
+    /* 웹소켓에서의 세션은 연결 정보를 의미한다 */
+    private static List<WebSocketSession> list = new ArrayList<>();
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        String payload = message.getPayload();
+        System.out.println("payload = " + payload);
+
+        for(WebSocketSession sess: list) {
+            // 메시지를 웹소켓 연결 중인 사람들에게 보낸다.
+            sess.sendMessage(message);
+        }
+    }
+
+    /* Client 접속 시 호출되는 메서드 */
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        list.add(session);
+        System.out.println("session = " + " 클라이언트 접속");
+    }
+
+
+    /* Client 접속 해제 시 호출되는 메서드 */
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        System.out.println("session = " + " 클라이언트 접속 해제");
+        list.remove(session);
+    }
+}
---
+++ src/main/java/com/chat/WebSocketConfig.java
@@ -0,0 +1,21 @@
+package com.chat;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+/* WebSocket 설정 */
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSocket // 웹소켓 활성화
+public class WebSocketConfig implements WebSocketConfigurer {
+    private final ChatHandler chatHandler;
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        // 웹소켓 접속을 위한 엔드포인트 등록
+        registry.addHandler(chatHandler, "/ws/chat").setAllowedOrigins("*");
+    }
+}
